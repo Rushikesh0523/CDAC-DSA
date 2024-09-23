45da7f077fa69d58c9be71da1a40a1ea7a1e25d5
Rushikesh Patil <rushipatilrp07@gmail.com>
2024-09-24 00:34:35 +0530

    Array Example 2

--- ArrayExamples2/src/com/demo/test/TestArray.java
+++ ArrayExamples2/src/com/demo/test/TestArray.java
@@ -5,23 +5,36 @@ import com.demo.service.ArrayService;
 public class TestArray {
 public static void main(String[] args) {
 	int[] arr=new int[5];
+	
 	ArrayService.acceptdata(arr);
+	
 	ArrayService.displaydata(arr);
+	
 	int num=ArrayService.findmax(arr);
+	
 	System.out.println("Maximum number is: "+num);
+	
 	num=ArrayService.findevenmaxnumber(arr);
-	if(num!=-1)
+	
+	if(num!=-1) {
 	  System.out.println("Maximum even number : "+num);
-	else
+	}
+	else {
 	   System.out.println("no even number present");
+	}
+	
 	ArrayService.findDigitsSumAll(arr);
+	
 	int[] arr1=ArrayService.findDuplicates(arr);
+	
 	if(arr1!=null) {
 			ArrayService.displaydata(arr1);
 	}else {
 		System.out.println("not found");
 	}
+	
 	ArrayService.rotatearray(4,arr);
+	
 	ArrayService.displaydata(arr);
 }
 
